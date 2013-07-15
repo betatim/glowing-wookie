831be679bd324bcc72256c36f1fabc6de50ce6f8
Thomas Bird <thomas.bird@cern.ch>
2013-07-15 14:21:32 +0200

    script for calculating the lumi of an nutple

---
+++ scripts/calculate_lumi.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+from math import sqrt
+
+import ROOT
+from ROOT import TFile
+
+tf= False
+if len(sys.argv)>1:
+  tf = TFile(sys.argv[1])
+else:
+  tf = TFile("/home/thomas/pipi_NTuple.root")
+
+lumi_tuple = tf.Get("GetIntegratedLuminosity/LumiTuple")
+
+lumi_tot = 0.
+lumi_err = 0.
+
+for data_file in lumi_tuple:
+  #print data_file.IntegratedLuminosity, "+/-", data_file.IntegratedLuminosityErr
+  lumi_tot += data_file.IntegratedLuminosity
+  le = data_file.IntegratedLuminosityErr
+  lumi_err +=  le * le
+  
+print "Total:", lumi_tot, "+/-", sqrt(lumi_err)
+
