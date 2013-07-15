7e086253df5d1f66b5666a2c9b245313932ade62
Thomas Bird <thomas.bird@cern.ch>
2013-07-15 14:20:17 +0200

    options files updates

--- ganga/emu-data-2012.py
+++ ganga/emu-data-2012.py
@@ -3,7 +3,8 @@ if len(sys.argv) > 1:
   polarity = sys.argv[1]
 else:
   sys.exit("You need to specify the magnet polarity")
-if len(sys.argv) > 2:
+  
+if len(sys.argv) > 2: # specify test after the polarity to run a test job
   test = True
   evtMax = 1000
 else:
@@ -33,6 +34,7 @@ dataset = j.application.readInputData("../data/stripping20r0p1_%s.py"%polarity)
 
 n_files_per_job = int(len(dataset)/50.) # try to have around 50 jobs
 if n_files_per_job > 100: n_files_per_job = 100 # max 100 allowed by dirac
+if n_files_per_job == 0: n_files_per_job = 1 # one file at least
 
 if test:
   j.inputdata = dataset[:40]
