3f1d8ae829a6063197417e612a2cc18ac13b9d40
Thomas Bird <thomas.bird@cern.ch>
2013-07-15 15:46:14 +0200

    adding plotspath

--- wookie/config.py
+++ wookie/config.py
@@ -5,5 +5,6 @@ import platform
 _at_cern = "lxplus" in platform.node()
 
 path = "/afs/cern.ch/work/t/tbird/demu/ntuples/"
+plotspath = "/afs/cern.ch/user/t/tbird/emu/glowing-wookie/fitters/"
 if os.getenv("USER") == "thead" and not _at_cern:
     path = "/tmp/ntuples/"
