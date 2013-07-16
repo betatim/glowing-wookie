6e0e14c2f500b5a567aceb0f0f5822f28858d673
Thomas Bird <thomas.bird@cern.ch>
2013-07-16 10:45:29 +0200

    change config file

--- wookie/config.py
+++ wookie/config.py
@@ -5,6 +5,6 @@ import platform
 _at_cern = "lxplus" in platform.node()
 
 path = "/afs/cern.ch/work/t/tbird/demu/ntuples/"
-plotspath = "/afs/cern.ch/user/t/tbird/emu/glowing-wookie/fitters/"
+plotspath = "/afs/cern.ch/user/t/tbird/emu/glowing-wookie/scripts/fitters/"
 if os.getenv("USER") == "thead" and not _at_cern:
     path = "/tmp/ntuples/"
